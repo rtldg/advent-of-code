337bd3cd9d487d91c273af02b7c2ec5b55a84da6
rtldg <rtldg@protonmail.com>
2022-12-05 07:57:54 +0000

    cleaner day05

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,60 +1,27 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_mut)]
-
 fn hello(ispart2: bool) {
-    let mut s = std::fs::read_to_string("input").unwrap();
+    let mut buckets: Vec<Vec<char>> = vec![Vec::new(); 9];
 
-    let mut buckets: Vec<Vec<char>> = Vec::with_capacity(9);
-    for i in 0..9 {
-        buckets.push(Vec::new());
-    }
-    let mut commands: Vec<(usize, usize, usize)> = Vec::new();
-    let mut reading_boxes = true;
-
-    for line in s.split('\n') {
-        if reading_boxes && line.as_bytes()[0] == b' ' && line.as_bytes()[1] == b'1' {
-            reading_boxes = false;
-            continue;
-        }
-        if line == "" {
-            continue;
-        };
-        if reading_boxes {
+    for line in std::fs::read_to_string("input").unwrap().split('\n') {
+        if line.contains("[") {
             for i in 0..9 {
                 let c = line.as_bytes()[i * 4 + 1];
                 if c != b' ' {
                     buckets[i].insert(0, c as char);
-                };
+                }
             }
-        } else {
-            let mut cmd: Vec<&str> = line.split(' ').skip(1).collect();
-            // println!("{:?}", cmd);
-            commands.push((
-                cmd[0].parse().unwrap(),
-                cmd[2].parse().unwrap(),
-                cmd[4].parse().unwrap(),
-            ));
-        }
-    }
-
-    // println!("{:?}", commands);
-    for cmd in &commands {
-        // println!("{:?}", buckets);
-        if ispart2 {
-            let len = buckets[cmd.1 - 1].len();
-            let elems: Vec<char> = buckets[cmd.1 - 1].drain((len - cmd.0)..len).collect();
-            buckets[cmd.2 - 1].extend(elems);
-        } else {
-            for i in 0..cmd.0 {
-                let elem = buckets[cmd.1 - 1].pop().unwrap();
-                buckets[cmd.2 - 1].push(elem);
+        } else if line.starts_with("move") {
+            let cmd: Vec<usize> = line.split(' ').filter_map(|s| s.parse().ok()).collect();
+            let len = buckets[cmd[1] - 1].len();
+            let mut elems: Vec<char> = buckets[cmd[1] - 1].drain((len - cmd[0])..len).collect();
+            if !ispart2 {
+                elems.reverse();
             }
+            buckets[cmd[2] - 1].extend(elems);
         }
     }
 
     for b in buckets {
-        print!("{}", b[b.len() - 1]);
+        print!("{}", b.last().unwrap());
     }
     println!("");
 }
