56b48984651cebb565d224f49a1b3040a41a37ad
rtldg <rtldg@protonmail.com>
2022-12-05 08:00:52 +0000

    'cleaner' day04

--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -2,42 +2,26 @@
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 
-fn part2() {
-    let mut answer: u32 = 0;
-    let mut s = std::fs::read_to_string("input").unwrap();
-    for line in s.split('\n') {
+fn main() {
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    for line in std::fs::read_to_string("input").unwrap().split('\n') {
         let pairs: Vec<Vec<i32>> = line
             .split(',')
             .map(|z| z.split('-').map(|y| y.parse::<i32>().unwrap()).collect())
             .collect();
+
+        if pairs[0][0] <= pairs[1][0] && pairs[0][1] >= pairs[1][1] {
+            p1 += 1;
+        } else if pairs[1][0] <= pairs[0][0] && pairs[1][1] >= pairs[0][1] {
+            p1 += 1;
+        }
 
         if pairs[0][0] <= pairs[1][1] && pairs[0][1] >= pairs[1][0] {
-            answer += 1;
+            p2 += 1;
         }
     }
-    println!("{}", answer);
-}
 
-fn part1() {
-    let mut answer: u32 = 0;
-    let mut s = std::fs::read_to_string("input").unwrap();
-    for line in s.split('\n') {
-        let pairs: Vec<Vec<i32>> = line
-            .split(',')
-            .map(|z| z.split('-').map(|y| y.parse::<i32>().unwrap()).collect())
-            .collect();
-        if pairs[0][0] <= pairs[1][0] && pairs[0][1] >= pairs[1][1] {
-            // println!("{:?}", pairs);
-            answer += 1;
-        } else if pairs[1][0] <= pairs[0][0] && pairs[1][1] >= pairs[0][1] {
-            // println!("{:?}", pairs);
-            answer += 1;
-        }
-    }
-    println!("{}", answer);
-}
-
-fn main() {
-    part1();
-    part2();
+    println!("{}\n{}", p1, p2);
 }
