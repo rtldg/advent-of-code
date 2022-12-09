0151465cb4561d8fd88a6b85e8a840738432756e
rtldg <rtldg@protonmail.com>
2022-12-09 10:03:02 +0000

    cleanup day09

--- 2022/day09/src/main.rs
+++ 2022/day09/src/main.rs
@@ -1,70 +1,43 @@
-fn distsq(a: [isize; 2], b: [isize; 2]) -> isize {
-    let dx = a[0] - b[0];
-    let dy = a[1] - b[1];
-    dx * dx + dy * dy
-}
-
-// shit. awful.
-fn new_pos(head: [isize; 2], tail: [isize; 2]) -> [isize; 2] {
-    let mut asdf = [(0, [0; 2]); 9];
-    let q = [-1, 0, 1];
-    for x in 0..3 {
-        for y in 0..3 {
-            let thing = [tail[0] + q[x], tail[1] + q[y]];
-            asdf[x * 3 + y] = (distsq(head, thing), thing);
-        }
-    }
-    asdf.sort();
-    asdf[0].1
-}
-
-fn hello(tails: usize) {
+fn main() {
     let input0 = std::fs::read_to_string("input").unwrap();
 
-    const SIZE: usize = 1000;
-    let mut visited: [[bool; SIZE]; SIZE] = [[false; SIZE]; SIZE];
+    const SIZE: usize = 800;
+    let mut visited = vec![vec![0u16; 800]; 800];
     let mut pos = [[(SIZE / 2) as isize; 2]; 10];
+    visited[SIZE / 2][SIZE / 2] = 0xFFFF;
 
     for line in input0.trim().split('\n') {
         let cmds: Vec<&str> = line.split(' ').collect();
         let steps = cmds[1].parse::<usize>().unwrap();
         for _ in 0..steps {
-            let original_pos = pos;
-
             match cmds[0] {
-                "U" => {
-                    pos[0][1] -= 1;
-                }
-                "D" => {
-                    pos[0][1] += 1;
-                }
-                "L" => {
-                    pos[0][0] -= 1;
-                }
-                "R" => {
-                    pos[0][0] += 1;
-                }
+                "U" => pos[0][1] -= 1,
+                "D" => pos[0][1] += 1,
+                "L" => pos[0][0] -= 1,
+                "R" => pos[0][0] += 1,
                 _ => (),
             }
 
-            if tails == 1 {
-                // This unfortunately didn't work with multiple tails...
-                if distsq(pos[0], pos[1]) >= 4 {
-                    pos[1] = original_pos[0];
-                }
-            } else {
-                for i in 0..tails {
-                    if distsq(pos[i], pos[i + 1]) >= 4 {
-                        let newpos = new_pos(pos[i], pos[i + 1]);
-                        // if newpos != pos[i+1] {
-                        //     println!("{} - {:?} -> {:?}", cmds[0], pos[i+1], newpos);
-                        // }
-                        pos[i + 1] = newpos;
-                    }
+            for i in 0..9 {
+                /*
+                [
+                       .   [-1,-1][ 0,-1][ 1,-1]   .
+                    [-1,-1]   .      .      .   [ 1,-1]
+                    [-1, 0]   .      o      .   [ 1, 0]
+                    [-1, 1]   .      .      .   [ 1, 1]
+                       .   [-1, 1][ 0, 1][ 1, 1]   .
+                ]
+                */
+
+                let dx = pos[i][0] - pos[i + 1][0];
+                let dy = pos[i][1] - pos[i + 1][1];
+
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    pos[i + 1][0] += dx.signum();
+                    pos[i + 1][1] += dy.signum();
+                    visited[pos[i + 1][0] as usize][pos[i + 1][1] as usize] |= 1 << (i + 1);
                 }
             }
-
-            visited[pos[tails][0] as usize][pos[tails][1] as usize] = true;
 
             // Print a pretty grid, which as useful for the part 2 test input...
             /*
@@ -104,15 +77,20 @@ fn hello(tails: usize) {
         }
     }
 
-    let answer: usize = visited
-        .iter()
-        .map(|v| v.iter().filter(|z| **z == true).count())
-        .sum();
+    let mut answer1 = 0;
+    let mut answer2 = 0;
 
-    println!("{}", answer);
-}
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            let z = visited[x][y];
+            if (z & (1 << 1)) != 0 {
+                answer1 += 1;
+            }
+            if (z & (1 << 9)) != 0 {
+                answer2 += 1;
+            }
+        }
+    }
 
-fn main() {
-    hello(1);
-    hello(9);
+    println!("{}\n{}", answer1, answer2);
 }
