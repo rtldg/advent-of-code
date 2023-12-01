d89b84c1adb53ec1b0f624c372ed5069afbfc959
rtldg <55846624+rtldg@users.noreply.github.com>
2023-12-01 05:49:42 +0000

    first iteration

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -12,7 +12,45 @@ fn main() {
         .map(str::to_string)
         .collect();
 
-    let (mut answer1, mut answer2) = (0, 0);
+    let mut numbers1 = vec![];
+    let mut numbers2 = vec![];
 
-    println!("{}\n{}", answer1, answer2);
+    let numberstrings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    for line in &input0 {
+        if line == "" {
+            continue;
+        }
+        let mut first_pos = 9999999999;
+        let mut first_val = 0;
+        let mut last_pos = 0;
+        let mut last_val = 0;
+        for (i,c) in line.chars().enumerate() {
+            if c.is_ascii_digit() {
+                if first_val == 0 {
+                    first_pos = i;
+                    first_val = c.to_digit(10).unwrap() as usize;
+                }
+                last_pos = i;
+                last_val = c.to_digit(10).unwrap() as usize;
+            }
+        }
+        numbers1.push(first_val*10 + last_val);
+        for t in numberstrings {
+            if let Some(x) = line.find(t) {
+                if x <= first_pos {
+                    first_pos = x;
+                    first_val = numberstrings.iter().position(|&v| v == t).unwrap() + 1;
+                }
+            }
+            if let Some(x) = line.rfind(t) {
+                if x >= last_pos {
+                    last_pos = x;
+                    last_val = numberstrings.iter().position(|&v| v == t).unwrap() + 1;
+                }
+            }
+        }
+        numbers2.push(first_val*10 + last_val);
+    }
+
+    println!("{}\n{}", numbers1.iter().sum::<usize>(), numbers2.iter().sum::<usize>());
 }
