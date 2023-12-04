fb3da94a91a737ef0468c0c89cd44fdc2a5a8c5a
rtldg <55846624+rtldg@users.noreply.github.com>
2023-12-04 05:16:14 +0000

    2023/day04 part 1

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -2,11 +2,13 @@
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 
-use std::error::Error;
+use std::{error::Error, collections::HashSet};
+
+use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_file = "test_input";
-    //let input_file = "input";
+    let input_file = "input";
     let input0 = std::fs::read_to_string(input_file)?;
 
     let input0: Vec<String> = std::fs::read_to_string(input_file)?
@@ -15,9 +17,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(str::to_string)
         .collect();
 
+    let mut total_point_worth = 0;
+
     for (lineid, line) in input0.iter().enumerate() {
+        let (winning, mine) = line.split(':').nth(1).unwrap().split('|').collect_tuple().unwrap();
+        let winning: HashSet<&str> = HashSet::from_iter(winning.split_ascii_whitespace());
+        let mine: HashSet<&str> = HashSet::from_iter(mine.split_ascii_whitespace());
+        /*let mut winning_hashset = HashSet::new();
+        let _ = winning.split_ascii_whitespace().map(|n| winning_hashset.insert(n.parse::<usize>())).collect();
+        for _ =*/
+        let matches = winning.intersection(&mine).count();
+        println!("{}", matches);
+
+        if matches > 0 {
+            total_point_worth += 1 << (matches-1);
+        }
     }
 
-    println!("\n{}\n{}", 0, 0);
+    println!("\n{}\n{}", total_point_worth, 0);
     Ok(())
 }
