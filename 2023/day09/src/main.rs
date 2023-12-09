b12e56c11f847245724233beffead80fc9f06e5f
rtldg <55846624+rtldg@users.noreply.github.com>
2023-12-09 05:25:16 +0000

    2023/day09 part 1

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -11,7 +11,7 @@ use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_file = "test_input";
-    // let input_file = "input";
+    let input_file = "input";
     let input0 = std::fs::read_to_string(input_file)?;
     let mut input0: Vec<String> = std::fs::read_to_string(input_file)?
         .trim()
@@ -19,6 +19,41 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(str::to_string)
         .collect();
 
-    println!("\n\n{:?}\n\n", 0);
+    // let mut things: Vec<Vec<i32>> = vec![];
+    let mut extrapolated_values = vec![];
+    for line in input0.iter() {
+        let mut history_idx = 0;
+        let mut history: Vec<Vec<i32>> = vec![line
+            .split_ascii_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect_vec()];
+
+        while history[history.len()-1].iter().find(|x| **x != 0).is_some() {
+            let mut current_row = vec![];
+            for i in 1..history[history.len()-1].len() {
+                current_row.push(history[history.len()-1][i] - history[history.len()-1][i-1]);
+            }
+            history.push(current_row);
+            // println!("{:?}", current_row);
+            //
+        }
+
+        for row in (1..history.len()).rev() {
+            let column = history[row].len()-1;
+            let extrapolated_value = history[row-1][column] + history[row][column];
+            extrapolated_values.push(extrapolated_value);
+            history[row].push(extrapolated_value);
+        }
+
+        // let mut extrapolated_value = 0;
+        // for row in history.iter() {
+        //     extrapolated_value += row[row.len()-1];
+        // }
+        // extrapolated_values.push(extrapolated_value);
+    }
+
+    // println!("{:?}", things);
+
+    println!("\n\n{:?}\n\n", extrapolated_values.iter().sum::<i32>());
     Ok(())
 }
