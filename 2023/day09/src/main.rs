55338a3f72fffdf52a0719448652b49f1f4e1626
rtldg <55846624+rtldg@users.noreply.github.com>
2023-12-09 05:45:43 +0000

    2023/day09 part 2

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -11,6 +11,7 @@ use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_file = "test_input";
+    // let input_file = "test_input_p2";
     let input_file = "input";
     let input0 = std::fs::read_to_string(input_file)?;
     let mut input0: Vec<String> = std::fs::read_to_string(input_file)?
@@ -19,7 +20,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(str::to_string)
         .collect();
 
-    // let mut things: Vec<Vec<i32>> = vec![];
     let mut extrapolated_values = vec![];
     for line in input0.iter() {
         let mut history_idx = 0;
@@ -28,32 +28,63 @@ fn main() -> Result<(), Box<dyn Error>> {
             .map(|x| x.parse().unwrap())
             .collect_vec()];
 
-        while history[history.len()-1].iter().find(|x| **x != 0).is_some() {
+        while history[history.len() - 1]
+            .iter()
+            .find(|x| **x != 0)
+            .is_some()
+        {
             let mut current_row = vec![];
-            for i in 1..history[history.len()-1].len() {
-                current_row.push(history[history.len()-1][i] - history[history.len()-1][i-1]);
+            for i in 1..history[history.len() - 1].len() {
+                current_row.push(history[history.len() - 1][i] - history[history.len() - 1][i - 1]);
             }
             history.push(current_row);
-            // println!("{:?}", current_row);
-            //
         }
 
         for row in (1..history.len()).rev() {
-            let column = history[row].len()-1;
-            let extrapolated_value = history[row-1][column] + history[row][column];
+            let column = history[row].len() - 1;
+            let extrapolated_value = history[row - 1][column] + history[row][column];
             extrapolated_values.push(extrapolated_value);
             history[row].push(extrapolated_value);
         }
-
-        // let mut extrapolated_value = 0;
-        // for row in history.iter() {
-        //     extrapolated_value += row[row.len()-1];
-        // }
-        // extrapolated_values.push(extrapolated_value);
     }
 
-    // println!("{:?}", things);
+    println!("\n\n{:?}\n\n", extrapolated_values.iter().sum::<i32>());
+
+    // part 2
+    // part 2
+    // part 2
+    // part 2
+
+    let mut extrapolated_values = vec![];
+    for line in input0.iter() {
+        let mut history_idx = 0;
+        let mut history: Vec<Vec<i32>> = vec![line
+            .split_ascii_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect_vec()];
+
+        while history[history.len() - 1]
+            .iter()
+            .find(|x| **x != 0)
+            .is_some()
+        {
+            let mut current_row = vec![];
+            for i in 1..history[history.len() - 1].len() {
+                current_row.push(history[history.len() - 1][i] - history[history.len() - 1][i - 1]);
+            }
+            history.push(current_row);
+        }
+
+        let x = history.len() - 1;
+        history[x].push(0);
+        for row in (1..history.len()).rev() {
+            let ex = history[row - 1][0] - history[row][0];
+            history[row - 1].insert(0, ex);
+        }
+        extrapolated_values.push(history[0][0]);
+    }
 
     println!("\n\n{:?}\n\n", extrapolated_values.iter().sum::<i32>());
+
     Ok(())
 }
