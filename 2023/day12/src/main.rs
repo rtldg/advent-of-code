18180a23a90d6d81dcea3f512794f63337f3d2c9
rtldg <55846624+rtldg@users.noreply.github.com>
2023-12-12 07:21:29 +0000

    2023/day12 skeleton

---
+++ 2023/day12/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(let_chains)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+
+use core::cmp::Ordering;
+use core::cmp::{max, min};
+use std::{collections::HashMap, collections::HashSet, error::Error};
+
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input_file = "test_input";
+    // let input_file = "test_input_p2";
+    // let input_file = "input";
+    let mut input0 = std::fs::read_to_string(input_file)?;
+    let mut input0: Vec<String> = std::fs::read_to_string(input_file)?
+        .trim()
+        .split('\n')
+        .map(str::to_string)
+        .collect();
+
+    for (lineid, line) in input0.iter().enumerate() {
+
+    }
+
+    println!("\n\n{:?}\n\n", 0);
+
+    Ok(())
+}
