6ce529924eb19dcc10066f243ddd28b195819cc6
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-04 14:32:11 +0000

    aoc2024day04p1

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -6,6 +6,7 @@
 
 use core::cmp::Ordering;
 use core::cmp::{max, min};
+use core::str;
 use std::{collections::HashMap, collections::HashSet, error::Error};
 
 use anyhow::Context;
@@ -13,12 +14,62 @@ use itertools::{Itertools, rev, sorted};
 
 fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
-	// let input_file = "input";
+	let input_file = "input";
 
-	let mut input0 = std::fs::read_to_string(input_file).context("failed to read input_file to string 1")?;
+	let mut grid: Vec<Vec<u8>> = std::fs::read_to_string(input_file)
+		.context("failed to read input_file to string 2")?
+		.trim()
+		.split('\n')
+		.map(|s| s.as_bytes().to_vec())
+		// .map(str::to_string)
+		.collect();
 
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
+
+	for line in &grid {
+		let line = unsafe { str::from_utf8_unchecked(line) };
+		answerp1 += line.matches("XMAS").count();
+		answerp1 += line.matches("SAMX").count();
+	}
+
+	let w = grid[0].len();
+	let h = grid.len();
+
+	// h-3 because we need space below for the verts
+	for y in 0..h - 3 {
+		for x in 0..w {
+			let down = [grid[y + 0][x], grid[y + 1][x], grid[y + 2][x], grid[y + 3][x]];
+			if down.eq(b"XMAS") || down.eq(b"SAMX") {
+				// println!("down");
+				answerp1 += 1;
+			}
+		}
+	}
+	for y in 3..h {
+		for x in 3..w {
+			let tl_to_br = [
+				grid[y - 3][x - 3],
+				grid[y - 2][x - 2],
+				grid[y - 1][x - 1],
+				grid[y - 0][x - 0],
+			];
+			if tl_to_br.eq(b"XMAS") || tl_to_br.eq(b"SAMX") {
+				// println!("tl_to_br");
+				answerp1 += 1;
+			}
+			let tr_to_bl = [
+				grid[y - 3][x - 0],
+				grid[y - 2][x - 1],
+				grid[y - 1][x - 2],
+				grid[y - 0][x - 3],
+			];
+			if tr_to_bl.eq(b"XMAS") || tr_to_bl.eq(b"SAMX") {
+				// println!("tr_to_bl");
+				answerp1 += 1;
+			}
+		}
+	}
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
