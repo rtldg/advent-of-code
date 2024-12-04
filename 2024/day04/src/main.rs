d907ad37bf8507feabf68fcda0413c913ec9a99b
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-04 13:52:20 +0000

    here we go again

---
+++ 2024/day04/src/main.rs
@@ -0,0 +1,26 @@
+#![feature(let_chains)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+
+use core::cmp::Ordering;
+use core::cmp::{max, min};
+use std::{collections::HashMap, collections::HashSet, error::Error};
+
+use anyhow::Context;
+use itertools::{Itertools, rev, sorted};
+
+fn main() -> anyhow::Result<()> {
+	let input_file = "test_input";
+	// let input_file = "input";
+
+	let mut input0 = std::fs::read_to_string(input_file).context("failed to read input_file to string 1")?;
+
+	let mut answerp1 = 0;
+	let mut answerp2 = 0;
+
+	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
+
+	Ok(())
+}
