3a782e27da452354058edde191c833e1248d50b8
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-07 14:17:43 +0000

    here we gogogo

---
+++ 2024/day07/src/main.rs
@@ -0,0 +1,33 @@
+#![feature(let_chains)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+
+use core::cmp::Ordering;
+use core::cmp::{max, min};
+use std::sync::atomic::AtomicU64;
+use std::{collections::HashMap, collections::HashSet, error::Error};
+
+use anyhow::Context;
+use itertools::{Itertools, rev, sorted};
+use rayon::prelude::*;
+
+fn main() -> anyhow::Result<()> {
+	let input_file = "test_input";
+	// let input_file = "input";
+
+	let mut answerp1 = 0;
+	let mut answerp2 = 0;
+
+	let mut lines: Vec<String> = std::fs::read_to_string(input_file)
+		.context("failed to read input_file to string 2")?
+		.trim()
+		.lines()
+		.map(str::to_string)
+		.collect();
+
+	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
+
+	Ok(())
+}
