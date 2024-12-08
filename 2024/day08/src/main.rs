f223d81e08010b9bfa626e1790a08abf5cdd54f0
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-08 06:03:11 +0000

    aoc2024day08p1

--- 2024/day08/src/main.rs
+++ 2024/day08/src/main.rs
@@ -15,20 +15,76 @@ use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
-	// let input_file = "input";
+	let input_file = "input";
 
-	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
-	let mut lines: Vec<String> = std::fs::read_to_string(input_file)
+	let mut grid: Vec<Vec<u8>> = std::fs::read_to_string(input_file)
 		.context("failed to read input_file to string 2")?
 		.trim()
 		.lines()
-		.map(str::to_string)
+		.map(|s| s.as_bytes().to_vec())
 		.collect();
 
-	for (lineidx, line) in lines.iter().enumerate() {
+	let h = grid.len();
+	let w = grid[0].len();
+
+	let mut antinodes = vec![vec![b'.'; w]; h];
+
+	let mut real_antinodes = HashMap::<(usize, usize, u8), bool>::new();
+
+	let mut do_antinodes = |anttype, starty, startx| {
+		for y in 0..h {
+			for x in 0..w {
+				if y == starty && x == startx {
+					continue;
+				}
+
+				let v = grid[y][x];
+				if v != anttype {
+					continue;
+				}
+
+				let antiy = if starty > y {
+					y - (starty - y)
+				} else {
+					y + (y - starty)
+				};
+				let antix = if startx > x {
+					x - (startx - x)
+				} else {
+					x + (x - startx)
+				};
+
+				if (0..h).contains(&antiy) && (0..w).contains(&antix) {
+					println!("plopping '{}' at y={} x={}", anttype as char, antiy, antix);
+					antinodes[antiy][antix] = anttype;
+					let _ = real_antinodes.insert((antiy, antix, anttype), true);
+				}
+			}
+		}
+	};
+
+	for y in 0..h {
+		for x in 0..w {
+			let antenna = grid[y][x];
+			if antenna.is_ascii_alphanumeric() {
+				do_antinodes(antenna, y, x);
+			}
+		}
 	}
+
+	let answerp1: usize = antinodes.iter().map(|r| r.iter().filter(|v| **v != b'.').count()).sum();
+
+	println!("");
+	for y in 0..h {
+		for x in 0..w {
+			print!("{}", antinodes[y][x] as char);
+		}
+		println!("");
+	}
+
+	// let answerp1 = real_antinodes.len();
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
