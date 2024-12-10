ce048930ca00ff7ffec077e26110aa6b327fcd45
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-10 14:49:01 +0000

    aoc2024day10p1

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -15,9 +15,8 @@ use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
-	// let input_file = "input";
+	let input_file = "input";
 
-	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
 	let mut grid: Vec<Vec<u8>> = std::fs::read_to_string(input_file)
@@ -28,7 +27,40 @@ fn main() -> anyhow::Result<()> {
 		// .map(str::to_string)
 		.collect();
 
-	let mut trailheads = HashMap::<(usize, usize), usize>::new();
+	let h = grid.len();
+	let w = grid[0].len();
+
+	let mut scores = HashMap::new();
+
+	fn traverse(grid: &Vec<Vec<u8>>, scores: &mut HashMap<((usize, usize), (usize, usize)), bool>, trailhead: (usize, usize), y: usize, x: usize) {
+		let directions = [[-1isize as usize, 0], [0, 1], [1, 0], [0, -1isize as usize]];
+		let current_height = grid[y][x];
+		for dir in &directions {
+			let nexty = y + dir[0];
+			let nextx = x + dir[1];
+			if (0..grid.len()).contains(&nexty) && (0..grid[0].len()).contains(&nextx) {
+				let next = grid[nexty][nextx];
+				if next.is_ascii_alphanumeric() && current_height + 1 == next {
+					if next == b'9' {
+						*scores.entry((trailhead, (nexty, nextx))).or_default() = true;
+						// println!("y={nexty} x={nextx}  - count={}", scores.values().count());
+					} else {
+						traverse(&grid, scores, trailhead, nexty, nextx);
+					}
+				}
+			}
+		}
+	}
+
+	for y in 0..h {
+		for x in 0..w {
+			if grid[y][x] == b'0' {
+				traverse(&grid, &mut scores, (y, x), y, x);
+			}
+		}
+	}
+
+	let answerp1: usize = scores.values().count();
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
