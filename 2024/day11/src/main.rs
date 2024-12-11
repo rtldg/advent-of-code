6b89f2f0babdbf07a1b30ec6399db0adf9b52be0
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-11 06:06:00 +0000

    aoc2024day11p1

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -15,7 +15,7 @@ use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
-	// let input_file = "input";
+	let input_file = "input";
 
 	let mut input: Vec<i64> = std::fs::read_to_string(input_file)
 		.context("failed to read input_file to string 2")?
@@ -26,6 +26,29 @@ fn main() -> anyhow::Result<()> {
 
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
+
+	for blink in 0..25 {
+		let mut i = 0;
+		while i < input.len() {
+			let stone = input[i];
+			if stone == 0 {
+				input[i] = 1;
+			} else {
+				let digits = stone.to_string();
+				if digits.len() & 1 == 0 {
+					let (l, r) = digits.split_at(digits.len() / 2);
+					input[i] = l.parse()?;
+					input.insert(i+1, r.parse()?);
+					i += 1;
+				} else {
+					input[i] *= 2024;
+				}
+			}
+			i += 1;
+		}
+	}
+
+	answerp1 = input.len();
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
