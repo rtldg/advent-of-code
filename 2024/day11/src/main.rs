4b2391d8837c9dfe53759ddc43c0824120b8fa04
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-12 04:10:28 +0000

    test crap

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -15,7 +15,7 @@ use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
-	let input_file = "input";
+	// let input_file = "input";
 
 	let mut input: Vec<i64> = std::fs::read_to_string(input_file)
 		.context("failed to read input_file to string 2")?
@@ -23,11 +23,14 @@ fn main() -> anyhow::Result<()> {
 		.split(' ')
 		.map(|s| s.parse().unwrap())
 		.collect();
+	let mut inputp2 = input.clone();
 
-	let mut answerp1 = 0;
+
 	let mut answerp2 = 0;
 
 	for blink in 0..25 {
+		println!("blink = {blink} | input.len() = {}", input.len());
+		// println!("{:?}", input[0]);
 		let mut i = 0;
 		while i < input.len() {
 			let stone = input[i];
@@ -48,7 +51,46 @@ fn main() -> anyhow::Result<()> {
 		}
 	}
 
-	answerp1 = input.len();
+	let mut answerp1 = input.len();
+
+	let mut counts = vec![];
+
+	for stone in 0..=9999 {
+		
+	}
+
+	let mut until_singles = vec![];
+	for stone in &inputp2 {
+		let mut stones = vec![*stone];
+		for blink in 0..75 {
+			println!("{blink}");
+			let mut i = 0;
+			while i < stones.len() {
+				let stone = stones[i];
+				if stone == 0 {
+					stones[i] = 1;
+				} else {
+					let digits = stone.to_string();
+					if digits.len() & 1 == 0 {
+						let (l, r) = digits.split_at(digits.len() / 2);
+						stones[i] = l.parse()?;
+						stones.insert(i+1, r.parse()?);
+						i += 1;
+					} else {
+						stones[i] *= 2024;
+					}
+				}
+				if stones.iter().find(|v| **v > 9).is_none() {
+					until_singles.push(blink);
+					println!("found single for {stone} at {blink}");
+					break;
+				}
+				i += 1;
+			}
+		}
+	}
+
+	println!("{:?}", until_singles);
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
