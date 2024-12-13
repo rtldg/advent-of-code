6e1d7c6383f1d30170cc842f134679c82e813942
rtldg <55846624+rtldg@users.noreply.github.com>
2024-12-13 03:43:41 +0000

    my fence tech is bad

--- 2024/day12/src/main.rs
+++ 2024/day12/src/main.rs
@@ -17,17 +17,119 @@ fn main() -> anyhow::Result<()> {
 	let input_file = "test_input";
 	// let input_file = "input";
 
-	let mut lines: Vec<String> = std::fs::read_to_string(input_file)
+	let mut grid: Vec<Vec<u8>> = std::fs::read_to_string(input_file)
 		.context("failed to read input_file to string 2")?
 		.trim()
 		.split('\n')
-		.map(str::to_string)
+		.map(|s| s.as_bytes().to_vec())
+		// .map(str::to_string)
 		.collect();
+
+	grid.insert(0, vec![0; grid[0].len()]);
+	grid.push(vec![0; grid[0].len()]);
+
+	for r in grid.iter_mut() {
+		r.insert(0, 0);
+		r.push(0);
+	}
+
+	let h = grid.len();
+	let w = grid[0].len();
 
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
-	for line in lines.iter().enumerate() {}
+	let mut plants = HashMap::<char, Vec<(usize, usize)>>::new();
+
+	let mut visited = vec![vec![false; w]; h];
+	let mut fences = vec![vec![false; w * 3]; h * 3];
+
+	fn traverse(
+		grid: &Vec<Vec<u8>>,
+		visited: &mut Vec<Vec<bool>>,
+		fences: &mut Vec<Vec<bool>>,
+		target_plant: u8,
+		y: usize,
+		x: usize,
+	) -> usize {
+		if visited[y][x] {
+			return 0;
+		}
+		visited[y][x] = true;
+
+		let mut area = 1;
+
+		// up
+		if grid[y - 1][x] != target_plant {
+			fences[y * 3 + 0][x * 3 + 0] = true;
+			fences[y * 3 + 0][x * 3 + 2] = true;
+		} else {
+			area += traverse(grid, visited, fences, target_plant, y - 1, x);
+		}
+		// down
+		if grid[y + 1][x] != target_plant {
+			fences[y * 3 + 2][x * 3 + 0] = true;
+			fences[y * 3 + 2][x * 3 + 2] = true;
+		} else {
+			area += traverse(grid, visited, fences, target_plant, y + 1, x);
+		}
+		// left
+		if grid[y][x - 1] != target_plant {
+			fences[y * 3 + 0][x * 3 + 0] = true;
+			fences[y * 3 + 2][x * 3 + 0] = true;
+		} else {
+			area += traverse(grid, visited, fences, target_plant, y, x - 1);
+		}
+		// right
+		if grid[y][x + 1] != target_plant {
+			fences[y * 3 + 0][x * 3 + 2] = true;
+			fences[y * 3 + 2][x * 3 + 2] = true;
+		} else {
+			area += traverse(grid, visited, fences, target_plant, y, x + 1);
+		}
+
+		area
+	}
+
+	for y in 1..h - 1 {
+		for x in 1..w - 1 {
+			if visited[y][x] {
+				continue;
+			}
+
+			let target_plant = grid[y][x];
+
+			let area = traverse(&grid, &mut visited, &mut fences, grid[y][x], y, x);
+			for row in 0..fences.len() {
+				for column in 0..fences[0].len() {
+					if fences[row][column] {
+						print!("+");
+					} else {
+						print!(".");
+					}
+				}
+				print!("\n");
+			}
+			print!("\n");
+
+			let fence_count = fences.iter().map(|r| r.iter().filter(|c| **c).count()).sum();
+			plants
+				.entry(target_plant as char)
+				.or_default()
+				.push((area, fence_count));
+
+			for r in fences.iter_mut() {
+				r.fill(false);
+			}
+		}
+	}
+
+	println!("{:#?}", plants);
+
+	let answerp1: usize = plants
+		.iter()
+		.map(|(plant, v)| v.iter().map(|(a, f)| *a * f).sum::<usize>())
+		.sum();
 
 	println!("\n\n{}\n\n{}\n\n", answerp1, answerp2);
 
