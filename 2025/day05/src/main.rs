a5a296e41d46c7c4d12a76e46e61a4ca8cdffa66
rtldg <rtldg@protonmail.com>
2025-12-05 05:10:25 +0000

    aoc2025day05p1

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -22,18 +22,32 @@ fn main() -> anyhow::Result<()> {
 
 fn swag(filename: &str) -> anyhow::Result<()> {
 	let mut input0 = std::fs::read_to_string(filename).context(format!("failed to read {filename} to string 1"))?;
-	let mut input0: Vec<String> = std::fs::read_to_string(filename)
-		.context(format!("failed to read {filename} to string 2"))?
+	let (fresh, available) = input0.split("\n\n").collect_tuple().unwrap();
+	let fresh: Vec<_> = fresh
 		.trim()
 		.split('\n')
-		.map(str::to_string)
+		.map(|line| {
+			let (l, r) = line
+				.split('-')
+				.map(|s| s.parse::<u64>().unwrap())
+				.collect_tuple()
+				.unwrap();
+			l..=r
+		})
 		.collect();
+	let available: Vec<u64> = available.trim().split('\n').map(|l| l.parse().unwrap()).collect();
 
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
-	for (lineno, line) in input0.iter().enumerate() {
-		//
+	for a in &available {
+		for range in &fresh {
+			if range.contains(a) {
+				//println!("{range:?} ({a}");
+				answerp1 += 1;
+				break;
+			}
+		}
 	}
 
 	println!("\n\n====== {filename}:\n{answerp1}\n{answerp2}\n");
