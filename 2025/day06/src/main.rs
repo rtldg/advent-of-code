443747f6ff90993eacb50cbf1c396a4160bd647e
rtldg <rtldg@protonmail.com>
2025-12-06 05:13:10 +0000

    aoc2025day06p1

--- 2025/day06/src/main.rs
+++ 2025/day06/src/main.rs
@@ -29,12 +29,36 @@ fn swag(filename: &str) -> anyhow::Result<()> {
 		.map(str::to_string)
 		.collect();
 
-	let mut answerp1 = 0;
-	let mut answerp2 = 0;
+	let mut answerp1 = 0u64;
+	let mut answerp2 = 0u64;
+
+	let mut numbers = vec![];
+	let mut operators = vec![];
+	let mut ncol = input0[0].trim().split_ascii_whitespace().count();
+	numbers.resize(ncol, vec![]);
 
 	for (lineno, line) in input0.iter().enumerate() {
-		//
+		let line = line.trim();
+		let mut contents = line.split_ascii_whitespace();
+		if line.as_bytes()[0].is_ascii_digit() {
+			for (i, v) in contents.enumerate() {
+				numbers[i].push(v.parse::<u64>().unwrap());
+			}
+		} else {
+			operators = contents.map(|s| s.as_bytes()[0]).collect();
+		}
 	}
+
+	for (i, op) in operators.iter().enumerate() {
+		if *op == b'+' {
+			answerp1 += numbers[i].iter().sum::<u64>();
+		} else if *op == b'*' {
+			answerp1 += numbers[i].iter().product::<u64>();
+		}
+	}
+
+	//dbg!(numbers);
+	//dbg!(operators);
 
 	println!("\n\n====== {filename}:\n{answerp1}\n{answerp2}\n");
 
