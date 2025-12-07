c8cf519cd86ab35c5d3465ad15f47962ee82fc7c
rtldg <rtldg@protonmail.com>
2025-12-07 05:18:55 +0000

    not quite it yet

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -2,6 +2,7 @@
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 #![allow(unused_imports)]
+#![allow(non_snake_case)]
 
 use core::cmp::Ordering;
 use core::cmp::{max, min};
@@ -20,21 +21,47 @@ fn main() -> anyhow::Result<()> {
 	Ok(())
 }
 
+fn start_splitter(rows: &[Vec<u8>], splitters: &mut HashSet<(usize, usize)>, starty: usize, startx: usize) {
+	for y in starty+1..rows.len() {
+		if rows[y][startx] == b'^' {
+			splitters.insert((y, startx));
+			if rows[y].len() > 1 {
+				start_splitter(rows, splitters, y, startx-1);
+			}
+			if startx < rows[y].len() {
+				start_splitter(rows, splitters, y, startx+1);
+			}
+		}
+	}
+}
+
 fn swag(filename: &str) -> anyhow::Result<()> {
 	let mut input0 = std::fs::read_to_string(filename).context(format!("failed to read {filename} to string 1"))?;
-	let mut input0: Vec<String> = std::fs::read_to_string(filename)
+	let mut input0: Vec<Vec<u8>> = std::fs::read_to_string(filename)
 		.context(format!("failed to read {filename} to string 2"))?
 		.trim()
 		.split('\n')
-		.map(str::to_string)
+		.map(|s| s.as_bytes().to_owned())
 		.collect();
 
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
-	for (lineno, line) in input0.iter().enumerate() {
-		//
+	let S = input0[0].iter().position(|c| *c == b'S').unwrap();
+
+	let mut splitters = HashSet::new();
+
+	start_splitter(&input0, &mut splitters, 0, S);
+
+	for (y, x) in &splitters {
+		input0[*y][*x] = b'!';
 	}
+	for line in input0 {
+		println!("{}", str::from_utf8(&line).unwrap());
+	}
+
+	//println!("{splitters:?}");
+	//println!("{}", splitters.len());
 
 	println!("\n\n====== {filename}:\n{answerp1}\n{answerp2}\n");
 
