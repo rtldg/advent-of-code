f1e3d9dde081609ff2cd37ae9c843cc67d091445
rtldg <rtldg@protonmail.com>
2025-12-10 05:37:00 +0000

    Not too practical to bruteforce. Could use a tracker of the current min, but shrug

--- 2025/day10/src/main.rs
+++ 2025/day10/src/main.rs
@@ -17,8 +17,16 @@ fn main() -> anyhow::Result<()> {
 		std::env::set_var("RUST_BACKTRACE", "full");
 	}
 	swag("test_input")?;
-	swag("input")?;
+	//swag("input")?;
 	Ok(())
+}
+
+#[derive(Debug)]
+struct Machine {
+	lights: u32,
+	light_goal: u32,
+	buttons: Vec<u32>,
+	joltage_requirements: Vec<u32>,
 }
 
 fn swag(filename: &str) -> anyhow::Result<()> {
@@ -33,8 +41,53 @@ fn swag(filename: &str) -> anyhow::Result<()> {
 	let mut answerp1 = 0;
 	let mut answerp2 = 0;
 
+	let mut machines = vec![];
+
 	for (lineno, line) in input0.iter().enumerate() {
-		//
+		let splits = line.split_ascii_whitespace().collect_vec();
+		//let joltagereq = splits[splits.len()-1];
+
+		let light_goal_s = &splits[0][1..splits[0].len()-1];
+		let mut light_goal_u32 = 0;
+		let lights = light_goal_s.len() as u32;
+		for (i, c) in light_goal_s.chars().enumerate() {
+			if c == '#' {
+				light_goal_u32 |= 1 << i;
+			}
+		}
+
+		let mut buttons = vec![];
+		for button in &splits[1..splits.len() - 1] {
+			let s = &button[1..button.len()-1];
+			for button in s.split(',') {
+				buttons.push(1 << button.parse::<u32>().unwrap());
+			}
+		}
+		let buttons = buttons.iter().cycle().cloned().take(buttons.len() * 5).collect();
+
+		machines.push(Machine {
+			lights: lights,
+			light_goal: light_goal_u32,
+			buttons: buttons,
+			joltage_requirements: vec![],
+		});
+	}
+
+	println!("{machines:?}");
+
+	for machine in machines {
+		let x = machine.buttons.iter().permutations(5).par_bridge().map(|perm| {
+			let mut state = 0;
+			for (i, b) in perm.iter().enumerate() {
+				state ^= *b;
+				if state == machine.light_goal {
+					return i + 1;
+				}
+			}
+			1000
+		}).collect_vec_list();
+		let x = x.iter().flatten().cloned().collect_vec();
+		println!("{}", x.iter().min().unwrap());
 	}
 
 	println!("\n\n====== {filename}:\n{answerp1}\n{answerp2}\n");
