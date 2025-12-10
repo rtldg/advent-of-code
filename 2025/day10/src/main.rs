3ad9acad1b7176b9708b084c54d82216fc5b2b6f
rtldg <rtldg@protonmail.com>
2025-12-10 04:57:22 +0000

    prep aoc2025day10

---
+++ 2025/day10/src/main.rs
@@ -0,0 +1,43 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+#![allow(non_snake_case)]
+
+use core::cmp::Ordering;
+use core::cmp::{max, min};
+use std::{collections::HashMap, collections::HashSet, error::Error};
+
+use anyhow::Context;
+use itertools::Itertools;
+use rayon::prelude::*;
+
+fn main() -> anyhow::Result<()> {
+	unsafe {
+		std::env::set_var("RUST_BACKTRACE", "full");
+	}
+	swag("test_input")?;
+	swag("input")?;
+	Ok(())
+}
+
+fn swag(filename: &str) -> anyhow::Result<()> {
+	let mut input0 = std::fs::read_to_string(filename).context(format!("failed to read {filename} to string 1"))?;
+	let mut input0: Vec<String> = std::fs::read_to_string(filename)
+		.context(format!("failed to read {filename} to string 2"))?
+		.trim()
+		.split('\n')
+		.map(str::to_string)
+		.collect();
+
+	let mut answerp1 = 0;
+	let mut answerp2 = 0;
+
+	for (lineno, line) in input0.iter().enumerate() {
+		//
+	}
+
+	println!("\n\n====== {filename}:\n{answerp1}\n{answerp2}\n");
+
+	Ok(())
+}
